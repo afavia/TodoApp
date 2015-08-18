1685f255a07014798e6fd55ee0d56b4eddcdf651
Anthony Favia <afavia@troygroup.com>
2015-08-18 15:47:26 -0400

    add labels to task activity

--- app/src/main/java/com/example/anthony/parsetodo/activities/TaskActivity.java
+++ app/src/main/java/com/example/anthony/parsetodo/activities/TaskActivity.java
@@ -3,8 +3,8 @@ package com.example.anthony.parsetodo.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -50,6 +50,14 @@ public class TaskActivity extends AppCompatActivity {
         mDueDate = (EditText) findViewById(R.id.due_date_edit_text);
         mDone = (CheckBox) findViewById(R.id.completed_check_box);
 
+        Button doneBtn = (Button) findViewById(R.id.done_button);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onDoneButtonClick();
+            }
+        });
+
         updateData();
     }
 
@@ -70,11 +78,20 @@ public class TaskActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+        setTaskData();
+
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(TASK_NUMBER,taskPos);
+        setResult(RESULT_CANCELED,returnIntent);
+        finish();
+    }
+
+    private void setTaskData() {
         mTask.setNotes(mNotes.getText().toString());
         mTask.setDescription(mDescription.getText().toString());
         mTask.setCompleted(mDone.isChecked());
 
-        SimpleDateFormat  format = new SimpleDateFormat();
+        SimpleDateFormat format = new SimpleDateFormat();
         try {
             Date date = format.parse(mDueDate.getText().toString());
             mTask.setDueDate(date);
@@ -84,32 +101,13 @@ public class TaskActivity extends AppCompatActivity {
 
         mTask.saveEventually();
         mController.updateTask(mTask, taskPos);
+    }
 
+    private void onDoneButtonClick() {
+        setTaskData();
         Intent returnIntent = new Intent();
         returnIntent.putExtra(TASK_NUMBER,taskPos);
-        setResult(RESULT_CANCELED,returnIntent);
+        setResult(RESULT_OK,returnIntent);
         finish();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_task, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
